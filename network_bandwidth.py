2c1b0eb8bcc4c01bd1bd4a825de477c2f00e42f9
wei.zhang <415108029@qq.com>
2019-07-16 10:48:05 +0800

    更新 code

---
+++ network_bandwidth.py
@@ -0,0 +1,18 @@
+# 网络带宽计算
+# 100/8
+# print(100/8)    # print() 函数表示输出
+
+# 变量赋值
+brandwidth = 100
+ratio = 8
+print(brandwidth/ratio)
+
+# 课后作业老师版本
+
+# 定义美元
+dollar = 100
+# 定义汇率
+exchange = 6.4696
+
+# 输出结果
+print('{dol}美元兑换的人民币数量为{yuan}'.format(dol=dollar, yuan=dollar * exchange))
